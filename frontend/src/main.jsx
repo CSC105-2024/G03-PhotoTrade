ed3c7a5772ac7a4b89556ec8475ab88371048acb
pun <PPbthunder@gmail.com>
2025-04-03 15:17:11 +0700

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,6 @@ const App = () => {
   if (path.includes(location.pathname)) {
     return <Layout>{useRoutes(routes)}</Layout>;
   }
-
   return useRoutes(routes);
 };
 
---
+++ frontend/src/reducer/auth.jsx
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const token = localStorage.getItem("token")
+  ? localStorage.getItem("token")
+  : null;
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    loading: false,
+    userInfo: {},
+    token,
+    error: null,
+    success: false,
+    isAuthenticated: false,
+  },
+  reducers: {
+    loginStatus: (state, action) => {
+      state.userInfo = action.payload
+      state.isAuthenticated = true;
+      // state.token = action.payload.token;
+      // localStorage.setItem("token", action.payload.token);
+    },
+    logoutStatus: (state) => {
+      state.userInfo = {}
+      state.isAuthenticated = false;
+      // state.token = null;
+      // localStorage.removeItem("token");
+    }
+  },
+});
+
+export const  { loginStatus, logoutStatus } = authSlice.actions
+export default authSlice.reducer;

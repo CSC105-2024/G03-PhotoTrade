4a6b0f00a31c10ede6d00457969b74172ca16e05
punkharb <ppbthunder@gmail.com>
2025-05-05 13:35:21 +0700

    [merge]

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,22 +1,48 @@
 import { Hono } from "hono";
-import { 
-    loginController, 
-    signUpController, 
+import {
+    loginController,
+    signUpController,
 } from "../controllers/user.controller.ts";
 import {
     likePhoto,
     countLike,
     unlikePhoto,
 } from "../controllers/user_Like.controller.ts";
-import { 
-    followUser, 
-    unfollowUser, 
+import {
+    followUser,
+    unfollowUser,
 } from "../controllers/user_Follow.controller.ts";
 import { auth } from "../middlewares/token.ts";
+import {
+    buyPhoto,
+    getphotohistorybyuser
+} from "../controllers/trade.controller.ts";
+import {
+    getallcategory,
+    addcategory,
+    getcategorybyid,
+    removecategory,
+} from "../controllers/category.controller.ts";
+
 
 const userRoutes = new Hono()
 
 userRoutes.post('/register', ...signUpController)
 userRoutes.post('/login', ...loginController)
+
+userRoutes.post('/like', likePhoto)
+userRoutes.delete('/unlike', unlikePhoto)
+userRoutes.get('/like/count', countLike)
+
+userRoutes.post('/follow', followUser)
+userRoutes.delete('/unfollow', unfollowUser)
+
+userRoutes.post('/buy', buyPhoto)
+userRoutes.get('/photohistory', getphotohistorybyuser)
+
+userRoutes.get('/categories', getallcategory)
+userRoutes.get('/categories/:id', getcategorybyid)
+userRoutes.put('/photo/category', addcategory)
+userRoutes.delete('/photo/category', removecategory)
 
 export default userRoutes
